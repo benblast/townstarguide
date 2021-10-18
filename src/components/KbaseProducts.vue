e126b113c632abd7bb7d0d9d6e18da60c59b67bd
Benblast <j.weiefors@gala.games>
2021-10-18 14:47:26 +0200

    TS site

--- src/components/KbaseProducts.vue
+++ src/components/KbaseProducts.vue
@@ -34,7 +34,7 @@
         >
           <td style="width: 5rem"><v-img :src="getFullUrl(item.FileUrl)" width="5rem"/></td>
           <td style="color: #ffd700; font-size: 1.5rem;">{{ item.Name }}</td>
-          <td><div style="display: flex; align-items: center;">{{ item.CityPoints }}<v-icon size="20px" color="#ffed87">mdi-star</v-icon></div></td>
+          <td><div style="display: flex; align-items: center;"><v-icon size="20px" color="#ffed87">mdi-star</v-icon>{{ item.CityPoints }}</div></td>
           <td><div style="display: flex; align-items: center;"><em style="color: green; font-style: normal;">$</em> {{ item.CityPrice }}</div></td>
           <td>{{ item.Value1 }} {{ item.Req1 }}<br>{{ item.Value2 }} {{ item.Req2 }}<br>{{ item.Value3 }} {{ item.Req3 }}</td>
           <td>
