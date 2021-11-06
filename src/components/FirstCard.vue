f8057a07189e5ff70c3f9e7d46f075c0ea859cc2
Benblast <j.weiefors@gala.games>
2021-11-06 18:43:24 +0100

    TS site

--- src/components/FirstCard.vue
+++ src/components/FirstCard.vue
@@ -3,19 +3,34 @@
   <div class="firstCardWrapper">
     <v-card class="firstCard" rounded>
       <v-card-text class="cardContent">
-        <h3>
-          WELCOME
-        </h3>
-        <v-divider class='my-3'></v-divider>
-        <p style="font-style: italic">
-          Thanks for putting up with all the bugs and stuff while we build!
-        </p>
-        <p style="font-size: 1rem!important;">
-          We aim to have all the Strategies and Tools that you need to play Town Star at a high level. Tools are being built. Guides are being written. TOWN is being Earned. Wool is being Rushed.
-        </p>
-        <h3 style="color: gold!important; font-size:2rem;">
-          Stay tuned.
-        </h3>
+        <div>
+          <h3>
+            WELCOME
+          </h3>
+          <v-divider class='my-3'></v-divider>
+          <p>
+            Struggling to reach the top of the leaderboards? Look no further, TownBlaster will help your gameplay reach new heights.
+          </p>
+          <p>
+            TownBlaster has everything. The strats, the tricks, the stats.
+          </p>
+          <ul>
+            <li>
+              Check out <router-link to="/strats">Strategies</router-link> to get info on good starts, end game builds and other goodies.
+            </li>
+            <li>
+              Need to know the stats of a product or a building? Head on over to our <router-link to="/kbase">Knowledge Base</router-link>!
+            </li>
+          </ul>
+        </div>
+        <div style="display: flex; justify-content: space-between; align-items: center;">
+          <h1 class='firstCardTitle' style="text-transform: uppercase; text-align: center;">
+            Level up<br>your gameplay
+          </h1>
+          <v-btn class="theButton" to="/strats" style="background: #636853;">
+            Go to Strategies
+          </v-btn>
+        </div>
       </v-card-text>
     </v-card>
   </div>
@@ -26,13 +41,8 @@
 export default {
   name: 'FirstCard',
   data: () => ({
-    hello: true
-  }),
-  methods: {
-    async postApi() {
 
-    }
-  }
+  })
 }
 </script>
 
@@ -44,54 +54,93 @@ h1, h2, h3 {
   margin-top: 10px;
 }
 p, ul, li {
-  font-size: 1.3rem;
-  line-height: 2rem;
+  font-size: 1rem;
   font-family: "Grotesk", Helvetica, sans-serif;
+  line-height: 1.2rem;
 }
+
 a {
   text-decoration: none;
   color: #636853;
-  filter: brightness(150%);
 }
+
 a:hover {
-  filter: brightness(250%);
-  text-decoration: underline;
 }
+
 .firstCardWrapper {
   display: flex;
-
-}
-
-.firstCard {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
+  align-items: center;
   height: 25rem;
 }
 
+.firstCard {
+
+  display: flex;
+  flex-direction: column;
+  height: 25rem;
+  width: 100%;
+}
+
 .cardContent {
-  padding: 2rem 2rem 0 2rem;
+  padding: 2rem;
+  display: flex;
+  height: 25rem;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
 .firstCardTitle {
   color: moccasin;
   font-weight: 800;
   font-size: 2rem;
-  display: flex;
-  justify-content: center;
+  line-height: 2rem;
 }
 
 .firstCardSide {
   display: flex;
   justify-content: center;
   padding: 0;
-  width: 12rem;
 }
-
-@media (max-width: 1400px) {
-  p, ul, li {
-    font-size: 1rem;
+@media (max-width: 1600px) {
+  p, ul {
+    line-height: 1rem;
   }
 }
 
+@media (max-width: 1400px) {
+  .firstCardWrapper {
+    height: 20rem;
+  }
+  .firstCard {
+    height: 20rem;
+  }
+  .firstCardTitle {
+    font-size: 1.4rem;
+  }
+  .theButton {
+    font-size: 0.6rem;
+    width: 8rem;
+  }
+}
+@media (max-width: 700px) {
+  .firstCard {
+    display: flex;
+    flex-direction: column;
+    height: auto;
+  }
+  .firstCardWrapper {
+    height: auto;
+  }
+  .firstCardTitle {
+    font-size: 1rem;
+    line-height: 1.5rem;
+  }
+  .firstCardSide {
+    display: none;
+  }
+  .cardContent {
+    padding: 1rem;
+    height: auto;
+  }
+}
 </style>
--- src/components/SecondCard.vue
+++ src/components/SecondCard.vue
@@ -1,61 +1,178 @@
+
+
 <template>
   <!--FIRST card START-->
   <div class="firstCardWrapper">
     <v-card class="firstCard" rounded>
-      <v-card-text class="cardContent">
-        <div style="margin-top: 1rem;">
-          <p>
-            Struggling to reach the top of the leaderboards? Look no further, TownBlaster will help your gameplay reach new heights.
-          </p>
-          <p>
-            TownBlaster has everything. The strats, the tricks, the stats.
-          </p>
-          <ul>
-            <li>
-              Check out <router-link to="/strats">Strategies</router-link> to get info on good starts, end game builds and other goodies.
-            </li>
-            <li>
-              Need to know the stats of a product or a building? Head on over to our <router-link to="/kbase">Knowledge Base</router-link>!
-            </li>
-          </ul>
+      <v-card class="priceCard">
+        <div style="width: 3rem; box-shadow: 3px 3px black; background-color: moccasin; filter: brightness(90%)">
+          <v-img :src="prices.eth.image.small" contain></v-img>
         </div>
-        <div style="display: flex; justify-content: space-between; align-items: center;">
-          <h1 class='firstCardTitle' style="text-transform: uppercase; text-align: center;">
-            Level up<br>your gameplay
-          </h1>
-          <v-btn class="theButton" to="/strats" style="background: #636853;">
-            Go to Strategies
-          </v-btn>
+        <div class="priceWrapper">
+          <div>
+            <div style="font-size: 0.8rem">
+              Price
+            </div>
+            <div class="theCoinInfo">
+              ${{ prices.eth.market_data.current_price.usd }}
+            </div>
+          </div>
+          <div>
+            <div style="font-size: 0.8rem">
+              24h Change
+            </div>
+            <div class="theCoinInfo">
+              {{ prices.eth.market_data.price_change_percentage_24h }}%
+            </div>
+          </div>
+          <div>
+            <div style="font-size: 0.8rem">
+              24h Low
+            </div>
+            <div class="theCoinInfo">
+              ${{ prices.eth.market_data.low_24h.usd }}
+            </div>
+            <div style="font-size: 0.8rem">
+              24h High
+            </div>
+            <div class="theCoinInfo">
+              ${{ prices.eth.market_data.high_24h.usd }}
+            </div>
+          </div>
         </div>
-      </v-card-text>
+      </v-card>
+      <v-card class="priceCard">
+        <div style="width: 3rem; box-shadow: 3px 3px black; background-color: moccasin; filter: brightness(90%)">
+          <v-img :src="prices.gala.image.small" contain></v-img>
+        </div>
+        <div class="priceWrapper">
+          <div>
+            <div style="font-size: 0.8rem">
+              Price
+            </div>
+            <div class="theCoinInfo">
+              ${{ prices.gala.market_data.current_price.usd }}
+            </div>
+          </div>
+
+          <div>
+            <div style="font-size: 0.8rem">
+              24h Change
+            </div>
+            <div class="theCoinInfo">
+              {{ prices.gala.market_data.price_change_percentage_24h }}%
+            </div>
+          </div>
+          <div>
+            <div style="font-size: 0.8rem">
+              24h Low
+            </div>
+            <div class="theCoinInfo">
+              ${{ prices.gala.market_data.low_24h.usd }}
+            </div>
+            <div style="font-size: 0.8rem">
+              24h High
+            </div>
+            <div class="theCoinInfo">
+              ${{ prices.gala.market_data.high_24h.usd }}
+            </div>
+          </div>
+        </div>
+      </v-card>
+      <v-card class="priceCard" style="margin-bottom: 0!important;">
+        <div style="width: 3rem; box-shadow: 3px 3px black; background-color: moccasin; filter: brightness(90%)">
+          <v-img :src="prices.town.image.small" contain></v-img>
+        </div>
+        <div class="priceWrapper">
+          <div>
+            <div style="font-size: 0.8rem">
+              Price
+            </div>
+            <div class="theCoinInfo">
+              ${{ prices.town.market_data.current_price.usd }}
+            </div>
+          </div>
+          <div>
+            <div style="font-size: 0.8rem">
+              24h Change
+            </div>
+            <div class="theCoinInfo">
+              {{ prices.town.market_data.price_change_percentage_24h }}%
+            </div>
+          </div>
+          <div>
+            <div style="font-size: 0.8rem">
+              24h Low
+            </div>
+            <div class="theCoinInfo">
+              ${{ prices.town.market_data.low_24h.usd }}
+            </div>
+            <div style="font-size: 0.8rem">
+              24h High
+            </div>
+            <div class="theCoinInfo">
+              ${{ prices.town.market_data.high_24h.usd }}
+            </div>
+          </div>
+        </div>
+      </v-card>
     </v-card>
   </div>
   <!--FIRST card END-->
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
+
 export default {
   name: 'SecondCard',
   data: () => ({
+    hello: true,
     prices: {
-      eth: 0,
-      gala: 0,
-      town: 0
+      eth: {},
+      gala: {},
+      town: {}
     }
   }),
+  methods: {
+    async postApi() {
+
+    }
+  },
   async mounted() {
-    const result = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=gala&vs_currencies=usd')
+    const result = await axios.get('https://api.coingecko.com/api/v3/coins/gala')
     console.log(result.data)
-    this.prices.gala = result.data.gala.usd
-    const result2 = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+    const result2 = await axios.get('https://api.coingecko.com/api/v3/coins/ethereum')
     console.log(result2.data)
-    this.prices.eth = result2.data.ethereum.usd
+    const result3 = await axios.get('https://api.coingecko.com/api/v3/coins/town-star')
+    console.log(result3.data)
+
+    this.prices.gala = result.data
+    this.prices.eth = result2.data
+    this.prices.town = result3.data
   }
 }
 </script>
 
 <style scoped>
+.priceWrapper {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+}
+.priceCard {
+  background-color: #636853;
+  margin-bottom: 1rem;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  text-align: center;
+}
+.theCoinInfo {
+  color: gold;
+  font-size: 1rem;
+}
 h1, h2, h3 {
   font-family: "Grotesk", Helvetica, sans-serif;
   color: moccasin!important;
@@ -63,94 +180,41 @@ h1, h2, h3 {
   margin-top: 10px;
 }
 p, ul, li {
-  font-size: 1rem;
+  font-size: 1.3rem;
+  line-height: 2rem;
   font-family: "Grotesk", Helvetica, sans-serif;
-  line-height: 1.2rem;
 }
-
 a {
   text-decoration: none;
   color: #636853;
+  filter: brightness(150%);
 }
-
 a:hover {
+  filter: brightness(250%);
+  text-decoration: underline;
 }
-
 .firstCardWrapper {
-  margin-top: 1rem;
   display: flex;
-  align-items: center;
-  height: 25rem;
+
 }
 
 .firstCard {
-
+  text-shadow: 1px 1px black;
+  -webkit-font-smoothing: antialiased;
+  margin-top: 1rem;
   display: flex;
-  flex-direction: column;
-  height: 25rem;
-  width: 100%;
-}
-
-.cardContent {
-  padding: 2rem;
-  display: flex;
-  height: 25rem;
   flex-direction: column;
   justify-content: space-between;
-}
-
-.firstCardTitle {
-  color: moccasin;
-  font-weight: 800;
-  font-size: 2rem;
-  line-height: 2rem;
-}
-
-.firstCardSide {
-  display: flex;
-  justify-content: center;
-  padding: 0;
-}
-@media (max-width: 1600px) {
-  p, ul {
-    line-height: 1rem;
-  }
+  width: 100%;
+  height: 25rem;
+  padding: 2rem 1rem 1rem 1rem;
 }
 
 @media (max-width: 1400px) {
-  .firstCardWrapper {
-    height: 20rem;
-  }
-  .firstCard {
-    height: 20rem;
-  }
-  .firstCardTitle {
-    font-size: 1.4rem;
-  }
-  .theButton {
-    font-size: 0.6rem;
-    width: 8rem;
-  }
-}
-@media (max-width: 700px) {
-  .firstCard {
-    display: flex;
-    flex-direction: column;
-    height: auto;
-  }
-  .firstCardWrapper {
-    height: auto;
-  }
-  .firstCardTitle {
+  p, ul, li {
     font-size: 1rem;
-    line-height: 1.5rem;
-  }
-  .firstCardSide {
-    display: none;
-  }
-  .cardContent {
-    padding: 1rem;
-    height: auto;
   }
 }
+
 </style>
+
